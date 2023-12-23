54c7fc36b41489853129f9a8c57fef0756e7760b
moiz <amkhatri89@gmail.com>
2023-12-23 19:57:13 +0500

    landing page

---
+++ Landing Page/app.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('aboutUsModal').style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('aboutUsModal').style.display = 'none';
+  }
