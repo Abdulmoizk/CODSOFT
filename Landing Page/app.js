0c5ef12227ad525a5d0ffa78ef256ba4f2b06bf5
moiz <amkhatri89@gmail.com>
2023-12-25 17:13:53 +0500

    updated

--- Landing Page/app.js
+++ Landing Page/app.js
@@ -5,3 +5,40 @@ function openModal() {
   function closeModal() {
     document.getElementById('aboutUsModal').style.display = 'none';
   }
+
+  let aboutInfo = document.getElementById("contactInfo");
+  let aboutBtn = document.getElementById("aboutBtn");  
+
+
+  aboutBtn.addEventListener("click",()=> {
+    
+    window.scrollTo({
+        top:aboutInfo.offsetTop,
+        behavior: "smooth", 
+    })
+  });
+  
+
+
+  /////////scrollToTop////////
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  
+    window.addEventListener('scroll', function() {
+      // Show the button when the page is scrolled down
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = 'block';
+      } else {
+        scrollToTopBtn.style.display = 'none';
+      }
+    });
+  
+    scrollToTopBtn.addEventListener('click', function() {
+      // Scroll to the top of the page with smooth behavior
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
